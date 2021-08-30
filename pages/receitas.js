e7ab54ed57716d8ed418cba9cd18c15e54fa9e5f
RamonzinhoBR40 <rranselmo4688@gmail.com>
2021-08-30 17:18:02 -0300

    imagem

--- pages/receitas.js
+++ pages/receitas.js
@@ -1,15 +1,20 @@
 import MENU from '../components/menu'
 import FOOTER from '../components/footer'
+import Image from 'next/image'
+import IMAGES from '../images/hamburguer_de_frango.jpg'
 
 function Receitas(){
     return (
         <>
             <MENU />
+           
+                <h1>My Homepage</h1>
+                <Image src={Image} alt="Picture of the author" />
+                <p>Welcome to my homepage!</p>
+            
             <div>RECEITAS</div>
             <FOOTER />
-
         </>
-
     )
 }
 
